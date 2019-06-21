ce79832eae36de26ee38fa23c7d8e24046500b85
Jamie Kelsch <jamiedk@gmail.com>
2019-06-21 08:03:11 -0600

    Input Polling
    https://www.youtube.com/watch?v=yuhNj8yGDJQ
    https://github.com/TheCherno/Hazel/commit/e1bb18d2c8e9c514d959c2eeb55d24af00d169ff

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -11,14 +11,7 @@ namespace Hazel {
 		WindowsWindow(const WindowProps& props);
 		virtual ~WindowsWindow();
 
-
-	
-
-
 		void OnUpdate() override;
-
-
-
 
 		inline unsigned int GetWidth() const override { return m_Data.Width; }
 		inline unsigned int GetHeight() const override { return m_Data.Height; }
@@ -27,10 +20,12 @@ namespace Hazel {
 		inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
+
+		inline virtual void* GetNativeWindow() const { return m_Window; }
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
-	
+	private:
 		GLFWwindow* m_Window;
 
 		struct WindowData
