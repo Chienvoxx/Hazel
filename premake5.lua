d1ef4067b5dbc81eddca5708d27f74a7f53bb6ed
Jamie Kelsch <jamiedk@gmail.com>
2019-06-26 08:48:32 -0600

    Maths
    Add glm
    https://www.youtube.com/watch?v=Idr1G1KyPNg

--- premake5.lua
+++ premake5.lua
@@ -12,9 +12,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
-IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
-IncludeDir["GLAD"] = "Hazel/vendor/glad/include"
-IncludeDir["IMGUI"] = "Hazel/vendor/imgui"
+IncludeDir["GLFW"] =	"Hazel/vendor/GLFW/include"
+IncludeDir["GLAD"] =	"Hazel/vendor/glad/include"
+IncludeDir["IMGUI"] =	"Hazel/vendor/imgui"
+IncludeDir["GLM"] =		"Hazel/vendor/glm"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/glad"
@@ -34,7 +35,9 @@ project "Hazel"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -43,7 +46,8 @@ project "Hazel"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.GLAD}",
-		"%{IncludeDir.IMGUI}"
+		"%{IncludeDir.IMGUI}",
+		"%{IncludeDir.GLM}"
 	}
 
 	links 
@@ -103,7 +107,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Hazel/vendor/spdlog/include",
-		"Hazel/src"
+		"Hazel/src",
+		"%{IncludeDir.GLM}"
 	}
 
 	links
