325d6bf184769eca4bea9d45b0eff86808b4f65f
Jamie Kelsch <jamiedk@gmail.com>
2020-04-12 22:13:04 -0600

    Basic ref-counting system to terminate glfw

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -9,7 +9,7 @@
 
 namespace Hazel {
 
-	static bool s_GLFWInitialized = false;
+	static uint8_t s_GLFWWindowCount = 0;
 
 	void GLFWErrorCallback(int error, const char* description)
 	{
@@ -43,14 +43,14 @@ namespace Hazel {
 		HZ_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWWindowCount == 0)
 		{
-			//TODO: glfwTerminate on system shutdown
+			HZ_CORE_INFO("Initializing GLFW");
 			int success = glfwInit();
 			HZ_CORE_ASSERT(success, "Could not initialize GLFW!");
 			glfwSetErrorCallback(GLFWErrorCallback);
-			s_GLFWInitialized = true;
 		}
+		++s_GLFWWindowCount;
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		m_Context = CreateScope<OpenGLContext>(m_Window);
@@ -176,6 +176,12 @@ namespace Hazel {
 	void WindowsWindow::Shutdown()
 	{
 		glfwDestroyWindow(m_Window);
+
+		if (--s_GLFWWindowCount == 0)
+		{
+			HZ_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 }
